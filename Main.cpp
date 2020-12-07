f204145c8f055a145a0c980713ef31c692f9a145
Space Fujisaki <41171313+spaceyspacestar@users.noreply.github.com>
2020-12-06 20:19:12 -0800

    Added comments for presentability

--- Main.cpp
+++ Main.cpp
@@ -3,16 +3,17 @@
 #include <stdio.h>
 #include <time.h>
 
-//Will this ever work x.x
-int APIENTRY WinMain( HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow ){
+int APIENTRY WinMain( HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow ) //For message boxes
+{
     
-    FreeConsole();
+    FreeConsole(); // This was for the other branch but we don't need this as of right now.
 
         int msgboxID = MessageBox(NULL, "Welcome to the Gay Meter!\nThis program will measure your gayness. \nAre you ready to roll?", "The Gay Meter", MB_ICONASTERISK | MB_YESNO);
 
-        switch (msgboxID){
+        switch (msgboxID) //These are for yes or no.
+        {
             case IDYES:
-            MessageBox(NULL, "Wow. You actual fucking dick. You just broke my gay meter!!\nBut hear me out, I have the results before it broke! You are really super fucking gay oh my fucking lord bro!\nLike you love to suck only schlongs and date hot sexy men.\nHowever... You can recover your gayness but I'm not gonna help you since you broke my gay meter you gay ass. :>\nWith that I give you the best of lucks trying to become straight after the results you got. Gay boy.", "The Gay Meter", MB_ICONERROR | MB_OK);
+            MessageBox(NULL, "Wow. You actual fucking dick. You just broke my gay meter!!\nBut hear me out, I have the results before it broke! You are really super fucking gay oh my fucking lord bro!\nLike you love to suck only schlongs and date hot sexy men.\nHowever... You can recover your gayness but I'm not gonna help you since you broke my gay meter you gay ass. :>\nWith that I give you the best of lucks trying to become straight after the results you got. Gay boy.", "The Gay Meter", MB_ICONERROR | MB_OK); //This is so long LMFAO
             break;
             case IDNO:
             MessageBox(NULL, "Ok bye.", "Fucking meanie. :<", MB_OK);
